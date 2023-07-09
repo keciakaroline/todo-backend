5556ad3d1e335e368d8b0b1b5fd15cf7c50e7fc0
StefanYohansson <sy.fen0@gmail.com>
2023-07-09 14:41:34 +0100

    add cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.db import models
 from app.db.database import engine
@@ -7,5 +8,17 @@ from app.routes.todos import todos_router
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router=todos_router, prefix="/todos")
