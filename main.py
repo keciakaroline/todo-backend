9e8d76bca0ef35fd40513f6f124c17d51368b1ed
Kecia Santos <keciakaroline@gmail.com>
2023-07-27 15:47:56 +0200

    reset

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-from app.db import models
-from app.db.database import engine
-from app.routes.todos import todos_router
-
-models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-origins = [
-    "*",
-]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-app.include_router(router=todos_router, prefix="/todos")
